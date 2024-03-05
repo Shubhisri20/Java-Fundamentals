8455d530a705ff6ce11d0689703d90fe85434033
Shubhi Srivastava <114871563+Shubhisri20@users.noreply.github.com>
2024-03-05 16:58:04 +0530

    Initial Commit

---
+++ AreaOfCircle.java
@@ -0,0 +1,9 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 8;
+        double pi = 3.1415;
+        double area = pi * radius * radius;
+        System.out.println("The area of circle is " +area );
+
+    }
+}
