50161c7fbab29f946607cd96f83df9aa6f05fb33
Shubhi Srivastava <114871563+Shubhisri20@users.noreply.github.com>
2024-03-15 00:02:19 +0530

    another commit

---
+++ ThreePointsOnALine.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ThreePointsOnALine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x1 = 1, x2 = 2, x3 = 3, y1 = 1, y2 = 2, y3 = 4;
+        double m1 = (y2-y1)/(x3-x2);
+        double m2 = (y3-y2)/(x3-x2);
+        if(m1 == m2){
+            System.out.println("The three line lie on the single line");
+        }
+        else{
+            System.out.println("They do not lie on the single line");
+        }
+    }
+}
